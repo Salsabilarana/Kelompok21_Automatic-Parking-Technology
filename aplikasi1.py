415462cfbb9fcb1535c52153209cb41b6c4cceb4
Yolanda Diandari <yolandadiandari@gmail.com>
2021-05-28 18:21:52 +0700

    update

--- aplikasi1.py
+++ aplikasi1.py
@@ -1,3 +1,10 @@
 print("================APLIKASI 1===================")
 #IMPORT WAKTU
+from datetime import datetime as dtm
 
+#IMPORT MODULE UNTUK CREATE BARCODE
+import pyqrcode
+
+#MEMBUAT QR CODE UNTUK PEMBAYARAN
+alat_bayar = pyqrcode.create("Terimakasih Telah Membayar")
+alat_bayar.png('pembayaran.png', scale=8)
