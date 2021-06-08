e35fd4fe110191df812afa6a6f1d49b2f0f04dfc
Tito Adam <perwirayudhaa@gmail.com>
2021-06-08 13:20:59 +0700

    update sampai hasil data yang diperoleh dari scan

--- Aplikasi2.py
+++ Aplikasi2.py
@@ -20,3 +20,28 @@ waktu_keluar = dtm.strptime(tgl_text,'%d-%b-%Y %H:%M:%S') # konversi string ke d
 #INPUT DATA KENDARAAN
 data_tipe_kendaraan = open("tipekendaraan.txt", "r")
 tipe_kendaraan = data_tipe_kendaraan.readline()
+
+#MENGHITUNG LAMA PARKIR
+hitung_lama_parkir = waktu_keluar - waktu_masuk
+detik_lama_parkir = hitung_lama_parkir.total_seconds()
+jam_lama_parkir = (detik_lama_parkir / 3600)
+if jam_lama_parkir == float(jam_lama_parkir) :
+    lama_parkir = int(jam_lama_parkir+1)
+else :
+    lama_parkir = int(jam_lama_parkir)
+##menghitung lama parkir jika lebih dari 1 hari
+hari1 = waktu_masuk
+hari2 = waktu_keluar
+total_hari = hari2 - hari1
+hari = total_hari.days
+
+#HASIL DATA YANG DIPEROLEH DARI SCAN
+print("==========================================================")
+print()
+print("Tipe Kendaraan Anda : ", (tipe_kendaraan))
+print("Waktu Masuk Anda : ", waktu_masuk)#, type(waktu_masuk))
+print("waktu Keluar Anda : ", waktu_keluar)#, type(waktu_keluar))
+print("Lama Parkir Anda : ", hitung_lama_parkir)
+#######print("Lama Parkir Yang Harus Dibayar : ", lama_parkir,"Jam")
+print()
+print("==========================================================")
